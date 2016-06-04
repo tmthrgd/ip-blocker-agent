7f7304ee8f36764d47b75a8101c91a8f0e58a28a
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-06-04 20:54:21 +0930

    Add IP blocker client

---
+++ client.go
@@ -0,0 +1,263 @@
+// Copyright 2016 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+package blocker
+
+/*
+#include <stdlib.h>          // For free
+#include <fcntl.h>           // For O_* constants
+#include <sys/stat.h>        // For mode constants
+#include <sys/mman.h>        // For shm_*
+#include <unistd.h>          // For sysconf and _SC_* constants
+
+#include "ngx_ip_blocker_shm.h"
+*/
+import "C"
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+var errInvalidSharedMem = errors.New("invalid shared memory")
+
+// Client is an IP blocker shared memory client.
+type Client struct {
+	fd int
+
+	addr unsafe.Pointer
+	size int64
+
+	mu sync.RWMutex
+
+	revision uint32
+}
+
+// Open returns a new IP blocker shared memory client
+// specified by name.
+func Open(name string) (*Client, error) {
+	nameC := C.CString(name)
+	defer C.free(unsafe.Pointer(nameC))
+
+	fd, err := C.shm_open(nameC, C.O_RDWR, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	var stat unix.Stat_t
+	if err = unix.Fstat(int(fd), &stat); err != nil {
+		unix.Close(int(fd))
+		return nil, err
+	}
+
+	size := stat.Size
+	if size < int64(headerSize) {
+		unix.Close(int(fd))
+		return nil, errInvalidSharedMem
+	}
+
+	addr, err := C.mmap(nil, C.size_t(size), C.PROT_READ|C.PROT_WRITE, C.MAP_SHARED, fd, 0)
+	if err != nil {
+		unix.Close(int(fd))
+		return nil, err
+	}
+
+	header := (*C.ngx_ip_blocker_shm_st)(addr)
+	lock := (*rwLock)(&header.lock)
+
+	client := &Client{
+		fd: int(fd),
+
+		addr: addr,
+		size: size,
+	}
+
+	lock.RLock()
+
+	client.revision = uint32(header.revision)
+
+	if err = unix.Fstat(int(fd), &stat); err != nil {
+		lock.RUnlock()
+
+		C.munmap(addr, C.size_t(size))
+		unix.Close(int(fd))
+		return nil, err
+	}
+
+	if stat.Size != size {
+		/* shm has changed since we mmaped it (unlikely but possible) */
+
+		/* RUnlock is called inside of remap iff an err is returned */
+		if err := client.remap(); err != nil {
+			unix.Close(int(fd))
+			return nil, err
+		}
+
+		header = (*C.ngx_ip_blocker_shm_st)(client.addr)
+		lock = (*rwLock)(&header.lock)
+	} else if !client.checkSharedMemory() {
+		lock.RUnlock()
+
+		C.munmap(addr, C.size_t(size))
+		unix.Close(int(fd))
+		return nil, errInvalidSharedMem
+	}
+
+	lock.RUnlock()
+	return client, nil
+}
+
+/* RLock must be held before calling remap */
+func (c *Client) remap() (err error) {
+	addr, size := c.addr, c.size
+	c.addr, c.size = nil, 0
+
+	var stat unix.Stat_t
+	if err := unix.Fstat(c.fd, &stat); err != nil {
+		goto err
+	}
+
+	c.addr, err = C.mmap(nil, C.size_t(stat.Size), C.PROT_READ|C.PROT_WRITE, C.MAP_SHARED, C.int(c.fd), 0)
+	if err != nil {
+		goto err
+	}
+
+	c.size = stat.Size
+
+	if !c.checkSharedMemory() {
+		err = errInvalidSharedMem
+		goto err
+	}
+
+	c.revision = uint32((*C.ngx_ip_blocker_shm_st)(c.addr).revision)
+
+	_, err = C.munmap(addr, C.size_t(size))
+	return
+
+err:
+	if c.size == 0 || c.size >= int64(headerSize) {
+		header := (*C.ngx_ip_blocker_shm_st)(addr)
+		lock := (*rwLock)(&header.lock)
+		lock.RUnlock()
+	} else {
+		fmt.Println("failed to release read lock")
+	}
+
+	C.munmap(addr, C.size_t(size))
+	return
+}
+
+func (c *Client) checkSharedMemory() bool {
+	header := (*C.ngx_ip_blocker_shm_st)(c.addr)
+
+	return c.size >= int64(headerSize) &&
+		c.size >= int64(headerSize)+int64(header.ip4.len+header.ip6.len+header.ip6route.len) &&
+		(header.ip4.len == 0 || uintptr(header.ip4.base) >= headerSize) &&
+		(header.ip6.len == 0 || uintptr(header.ip6.base) >= headerSize) &&
+		(header.ip6route.len == 0 || uintptr(header.ip6route.base) >= headerSize) &&
+		int64(uintptr(header.ip4.base)+uintptr(header.ip4.len)) <= c.size &&
+		int64(uintptr(header.ip6.base)+uintptr(header.ip6.len)) <= c.size &&
+		int64(uintptr(header.ip6route.base)+uintptr(header.ip6route.len)) <= c.size &&
+		header.ip4.len%4 == 0 &&
+		header.ip6.len%16 == 0 &&
+		header.ip6route.len%8 == 0
+}
+
+// Contains returns a boolean indicating whether the
+// IP address is in the blocklist.
+func (c *Client) Contains(ip net.IP) (bool, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	if c.addr == nil || c.size < int64(headerSize) {
+		return false, errors.New("invalid state")
+	}
+
+	header := (*C.ngx_ip_blocker_shm_st)(c.addr)
+	lock := (*rwLock)(&header.lock)
+
+	lock.RLock()
+
+	if c.revision != uint32(header.revision) {
+		/* RUnlock is called inside of remap iff an error is returned */
+		if err := c.remap(); err != nil {
+			return false, err
+		}
+
+		header = (*C.ngx_ip_blocker_shm_st)(c.addr)
+		lock = (*rwLock)(&header.lock)
+	}
+
+	defer lock.RUnlock()
+
+	if ip4 := ip.To4(); ip4 != nil {
+		ip = ip4
+
+		if header.ip4.len == 0 {
+			return false, nil
+		}
+
+		searcher := newBinarySearcher(net.IPv4len, nil)
+		ip4Base := (*[1 << 30]byte)(unsafe.Pointer(uintptr(c.addr) + uintptr(header.ip4.base)))
+		searcher.Data = ip4Base[:header.ip4.len:header.ip4.len]
+
+		return searcher.Contains(ip), nil
+	} else if ip6 := ip.To16(); ip6 != nil {
+		ip = ip6
+
+		if header.ip6route.len != 0 {
+			searcher := newBinarySearcher(net.IPv6len/2, nil)
+			ip6rBase := (*[1 << 30]byte)(unsafe.Pointer(uintptr(c.addr) + uintptr(header.ip6route.base)))
+			searcher.Data = ip6rBase[:header.ip6route.len:header.ip6route.len]
+
+			if searcher.Contains(ip[:net.IPv6len/2]) {
+				return true, nil
+			}
+		}
+
+		if header.ip6.len == 0 {
+			return false, nil
+		}
+
+		searcher := newBinarySearcher(net.IPv6len, nil)
+		ip6Base := (*[1 << 30]byte)(unsafe.Pointer(uintptr(c.addr) + uintptr(header.ip6.base)))
+		searcher.Data = ip6Base[:header.ip6.len:header.ip6.len]
+
+		return searcher.Contains(ip), nil
+	} else {
+		return false, &net.AddrError{Err: "invalid IP address", Addr: ip.String()}
+	}
+}
+
+// Close closes the blockers shared memory and
+// releases the file descriptor.
+func (c *Client) Close() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.addr != nil {
+		if _, err := C.munmap(c.addr, C.size_t(c.size)); err != nil {
+			return err
+		}
+
+		c.addr = nil
+		c.size = 0
+	}
+
+	if c.fd != -1 {
+		if err := unix.Close(c.fd); err != nil {
+			return err
+		}
+
+		c.fd = -1
+	}
+
+	return nil
+}
---
+++ ip-blocker-client/client.go
@@ -0,0 +1,80 @@
+// Copyright 2016 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+// An efficient shared memory IP blocking system for nginx.
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	blocker "github.com/tmthrgd/ip-blocker-agent"
+)
+
+func main() {
+	var name string
+	flag.StringVar(&name, "name", "/ngx-ip-blocker", "the shared memory name")
+
+	flag.Parse()
+
+	if len(name) == 0 {
+		fmt.Println("-name cannot be empty")
+		os.Exit(1)
+	}
+
+	block, err := blocker.Open(name)
+	if err != nil {
+		if blocker.IsNotExist(err) {
+			fmt.Println(err)
+			os.Exit(1)
+		} else {
+			panic(err)
+		}
+	}
+
+	defer block.Close()
+
+	stdin := bufio.NewScanner(os.Stdin)
+
+	for stdin.Scan() {
+		line := stdin.Text()
+		if len(line) == 0 {
+			fmt.Printf("invalid input: %s\n", line)
+			continue
+		}
+
+		switch line[0] {
+		case 'q':
+			fallthrough
+		case 'Q':
+			if len(line) == 1 || strings.EqualFold(line, "quit") {
+				return
+			}
+
+			fmt.Printf("invalid input: %s\n", line)
+		default:
+			ip := net.ParseIP(line)
+			if ip == nil {
+				fmt.Printf("invalid ip address: %s\n", line)
+				continue
+			}
+
+			has, err := block.Contains(ip)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("%t\n", has)
+		}
+	}
+
+	if err = stdin.Err(); err != nil {
+		panic(err)
+	}
+}
