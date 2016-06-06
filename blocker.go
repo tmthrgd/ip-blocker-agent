tmthrgd/ip-blocker-agent

commit 78fb4a2016c9163fa5096984b3025c5ee667fff8

Entirely remove cgo dependency on linux

blocker.go
@@ -3,28 +3,79 @@
 // Modified BSD License license that can be found in
 // the LICENSE file.
 
+// +build !linux !386,!amd64
+
 // Package blocker is an efficient shared memory IP
 // blocking system for nginx.
 //
 // See https://github.com/tmthrgd/nginx-ip-blocker
 package blocker
 
-//#include "ngx_ip_blocker_shm.h"
+/*
+#include <stdint.h>         // For int32_t
+#include <semaphore.h>      // For sem_t
+
+#define IP_BLOCKER_MAX_READERS (1 << 30)
+
+typedef struct {
+	sem_t Sem;
+} ip_blocker_mutex_st;
+
+typedef struct {
+	ip_blocker_mutex_st W;        // held if there are pending writers
+	sem_t WriterSem;              // semaphore for writers to wait for completing readers
+	sem_t ReaderSem;              // semaphore for readers to wait for completing writers
+	volatile int32_t ReaderCount; // number of pending readers
+	volatile int32_t ReaderWait;  // number of departing readers
+} ip_blocker_rwlock_st;
+
+typedef struct {
+	volatile size_t Base;
+	volatile size_t Len;
+} ip_blocker_ip_block_st;
+
+typedef struct {
+	uint32_t Version;
+	volatile uint32_t Revision;
+
+	ip_blocker_rwlock_st Lock;
+
+	ip_blocker_ip_block_st IP4, IP6, IP6Route;
+} ip_blocker_shm_st;
+*/
 import "C"
 
-import (
-	"errors"
-	"unsafe"
-)
+import "errors"
 
 // ErrClosed will be returned on attempts to call
 // methods on a closed server or client.
 var ErrClosed = errors.New("shared memory closed")
 
-const headerSize = unsafe.Sizeof(C.ngx_ip_blocker_shm_st{})
+type mutex C.ip_blocker_mutex_st
 
-type shmHeader C.ngx_ip_blocker_shm_st
+type rwLock C.ip_blocker_rwlock_st
+
+type ipBlock C.ip_blocker_ip_block_st
+
+type shmHeader C.ip_blocker_shm_st
 
 func (h *shmHeader) rwLocker() *rwLock {
-	return (*rwLock)(&h.lock)
+	return (*rwLock)(&h.Lock)
 }
+
+func (h *shmHeader) setBlocks(ip4, ip4len, ip6, ip6len, ip6r, ip6rlen int) {
+	h.IP4.Base = C.size_t(ip4)
+	h.IP4.Len = C.size_t(ip4len)
+
+	h.IP6.Base = C.size_t(ip6)
+	h.IP6.Len = C.size_t(ip6len)
+
+	h.IP6Route.Base = C.size_t(ip6r)
+	h.IP6Route.Len = C.size_t(ip6rlen)
+}
+
+const (
+	headerSize = C.sizeof_ip_blocker_shm_st
+
+	rwLockMaxReaders = C.IP_BLOCKER_MAX_READERS
+)

blocker_linux_386.go
@@ -1,0 +1,55 @@
+// Created by cgo -godefs - DO NOT EDIT
+// cgo -godefs blocker.go
+
+package blocker
+
+import "errors"
+
+var ErrClosed = errors.New("shared memory closed")
+
+type mutex struct {
+	Sem [16]byte
+}
+
+type rwLock struct {
+	W           mutex
+	WriterSem   [16]byte
+	ReaderSem   [16]byte
+	ReaderCount int32
+	ReaderWait  int32
+}
+
+type ipBlock struct {
+	Base uint32
+	Len  uint32
+}
+
+type shmHeader struct {
+	Version  uint32
+	Revision uint32
+	Lock     rwLock
+	IP4      ipBlock
+	IP6      ipBlock
+	IP6Route ipBlock
+}
+
+func (h *shmHeader) rwLocker() *rwLock {
+	return (*rwLock)(&h.Lock)
+}
+
+func (h *shmHeader) setBlocks(ip4, ip4len, ip6, ip6len, ip6r, ip6rlen int) {
+	h.IP4.Base = uint32(ip4)
+	h.IP4.Len = uint32(ip4len)
+
+	h.IP6.Base = uint32(ip6)
+	h.IP6.Len = uint32(ip6len)
+
+	h.IP6Route.Base = uint32(ip6r)
+	h.IP6Route.Len = uint32(ip6rlen)
+}
+
+const (
+	headerSize = 0x58
+
+	rwLockMaxReaders = 0x40000000
+)

blocker_linux_amd64.go
@@ -1,0 +1,55 @@
+// Created by cgo -godefs - DO NOT EDIT
+// cgo -godefs blocker.go
+
+package blocker
+
+import "errors"
+
+var ErrClosed = errors.New("shared memory closed")
+
+type mutex struct {
+	Sem [32]byte
+}
+
+type rwLock struct {
+	W           mutex
+	WriterSem   [32]byte
+	ReaderSem   [32]byte
+	ReaderCount int32
+	ReaderWait  int32
+}
+
+type ipBlock struct {
+	Base uint64
+	Len  uint64
+}
+
+type shmHeader struct {
+	Version  uint32
+	Revision uint32
+	Lock     rwLock
+	IP4      ipBlock
+	IP6      ipBlock
+	IP6Route ipBlock
+}
+
+func (h *shmHeader) rwLocker() *rwLock {
+	return (*rwLock)(&h.Lock)
+}
+
+func (h *shmHeader) setBlocks(ip4, ip4len, ip6, ip6len, ip6r, ip6rlen int) {
+	h.IP4.Base = uint64(ip4)
+	h.IP4.Len = uint64(ip4len)
+
+	h.IP6.Base = uint64(ip6)
+	h.IP6.Len = uint64(ip6len)
+
+	h.IP6Route.Base = uint64(ip6r)
+	h.IP6Route.Len = uint64(ip6rlen)
+}
+
+const (
+	headerSize = 0xa0
+
+	rwLockMaxReaders = 0x40000000
+)

client.go
@@ -56,7 +56,7 @@ func Open(name string) (*Client, error) {
 	}
 
 	header := (*shmHeader)(unsafe.Pointer(&data[0]))
-	if header.version != 1 {
+	if header.Version != 1 {
 		file.Close()
 		return nil, errInvalidSharedMem
 	}
@@ -70,7 +70,7 @@ func Open(name string) (*Client, error) {
 	lock := header.rwLocker()
 	lock.RLock()
 
-	client.revision = uint32(header.revision)
+	client.revision = uint32(header.Revision)
 
 	stat, err = file.Stat()
 	if err != nil {
@@ -121,7 +121,7 @@ func (c *Client) remap(force bool) (err error) {
 		}
 
 		header := (*shmHeader)(unsafe.Pointer(&c.data[0]))
-		if c.revision == uint32(header.revision) {
+		if c.revision == uint32(header.Revision) {
 			return nil
 		}
 	}
@@ -144,7 +144,7 @@ func (c *Client) remap(force bool) (err error) {
 		goto err
 	}
 
-	c.revision = uint32((*shmHeader)(unsafe.Pointer(&c.data[0])).revision)
+	c.revision = uint32((*shmHeader)(unsafe.Pointer(&c.data[0])).Revision)
 
 	err = syscall.Munmap(data)
 	return
@@ -170,20 +170,20 @@ func (c *Client) checkSharedMemory() bool {
 
 	const maxInt = int(^uint(0) >> 1)
 	return len(c.data) >= int(headerSize) &&
-		uintptr(headerSize)+uintptr(header.ip4.len+header.ip6.len+header.ip6route.len) <= uintptr(maxInt) &&
-		len(c.data) >= int(headerSize)+int(header.ip4.len+header.ip6.len+header.ip6route.len) &&
-		(header.ip4.len == 0 || uintptr(header.ip4.base) >= headerSize) &&
-		(header.ip6.len == 0 || uintptr(header.ip6.base) >= headerSize) &&
-		(header.ip6route.len == 0 || uintptr(header.ip6route.base) >= headerSize) &&
-		uintptr(header.ip4.base)+uintptr(header.ip4.len) <= uintptr(maxInt) &&
-		uintptr(header.ip6.base)+uintptr(header.ip6.len) <= uintptr(maxInt) &&
-		uintptr(header.ip6route.base)+uintptr(header.ip6route.len) <= uintptr(maxInt) &&
-		int(uintptr(header.ip4.base)+uintptr(header.ip4.len)) <= len(c.data) &&
-		int(uintptr(header.ip6.base)+uintptr(header.ip6.len)) <= len(c.data) &&
-		int(uintptr(header.ip6route.base)+uintptr(header.ip6route.len)) <= len(c.data) &&
-		header.ip4.len%4 == 0 &&
-		header.ip6.len%16 == 0 &&
-		header.ip6route.len%8 == 0
+		uintptr(headerSize)+uintptr(header.IP4.Len+header.IP6.Len+header.IP6Route.Len) <= uintptr(maxInt) &&
+		len(c.data) >= int(headerSize)+int(header.IP4.Len+header.IP6.Len+header.IP6Route.Len) &&
+		(header.IP4.Len == 0 || uintptr(header.IP4.Base) >= headerSize) &&
+		(header.IP6.Len == 0 || uintptr(header.IP6.Base) >= headerSize) &&
+		(header.IP6Route.Len == 0 || uintptr(header.IP6Route.Base) >= headerSize) &&
+		uintptr(header.IP4.Base)+uintptr(header.IP4.Len) <= uintptr(maxInt) &&
+		uintptr(header.IP6.Base)+uintptr(header.IP6.Len) <= uintptr(maxInt) &&
+		uintptr(header.IP6Route.Base)+uintptr(header.IP6Route.Len) <= uintptr(maxInt) &&
+		int(uintptr(header.IP4.Base)+uintptr(header.IP4.Len)) <= len(c.data) &&
+		int(uintptr(header.IP6.Base)+uintptr(header.IP6.Len)) <= len(c.data) &&
+		int(uintptr(header.IP6Route.Base)+uintptr(header.IP6Route.Len)) <= len(c.data) &&
+		header.IP4.Len%4 == 0 &&
+		header.IP6.Len%16 == 0 &&
+		header.IP6Route.Len%8 == 0
 }
 
 // Contains returns a boolean indicating whether the
@@ -205,7 +205,7 @@ func (c *Client) Contains(ip net.IP) (bool, error) {
 
 	lock.RLock()
 
-	if c.revision != uint32(header.revision) {
+	if c.revision != uint32(header.Revision) {
 		/* RUnlock is called inside of remap iff an error is returned */
 		if err := c.remap(false); err != nil {
 			return false, err
@@ -220,32 +220,32 @@ func (c *Client) Contains(ip net.IP) (bool, error) {
 	if ip4 := ip.To4(); ip4 != nil {
 		ip = ip4
 
-		if header.ip4.len == 0 {
+		if header.IP4.Len == 0 {
 			return false, nil
 		}
 
 		searcher := newBinarySearcher(net.IPv4len, nil)
-		searcher.Data = c.data[header.ip4.base : int(header.ip4.base)+int(header.ip4.len)]
+		searcher.Data = c.data[header.IP4.Base : int(header.IP4.Base)+int(header.IP4.Len)]
 
 		return searcher.Contains(ip), nil
 	} else if ip6 := ip.To16(); ip6 != nil {
 		ip = ip6
 
-		if header.ip6route.len != 0 {
+		if header.IP6Route.Len != 0 {
 			searcher := newBinarySearcher(net.IPv6len/2, nil)
-			searcher.Data = c.data[header.ip6route.base : int(header.ip6route.base)+int(header.ip6route.len)]
+			searcher.Data = c.data[header.IP6Route.Base : int(header.IP6Route.Base)+int(header.IP6Route.Len)]
 
 			if searcher.Contains(ip[:net.IPv6len/2]) {
 				return true, nil
 			}
 		}
 
-		if header.ip6.len == 0 {
+		if header.IP6.Len == 0 {
 			return false, nil
 		}
 
 		searcher := newBinarySearcher(net.IPv6len, nil)
-		searcher.Data = c.data[header.ip6.base : int(header.ip6.base)+int(header.ip6.len)]
+		searcher.Data = c.data[header.IP6.Base : int(header.IP6.Base)+int(header.IP6.Len)]
 
 		return searcher.Contains(ip), nil
 	} else {
@@ -301,8 +301,8 @@ func (c *Client) Count() (ip4, ip6, ip6routes int, err error) {
 
 	header := (*shmHeader)(unsafe.Pointer(&c.data[0]))
 
-	ip4 = int(header.ip4.len / net.IPv4len)
-	ip6 = int(header.ip6.len / net.IPv6len)
-	ip6routes = int(header.ip6route.len / (net.IPv6len / 2))
+	ip4 = int(header.IP4.Len / net.IPv4len)
+	ip6 = int(header.IP6.Len / net.IPv6len)
+	ip6routes = int(header.IP6Route.Len / (net.IPv6len / 2))
 	return
 }

server.go
@@ -5,8 +5,6 @@
 
 package blocker
 
-import "C"
-
 import (
 	"errors"
 	"net"
@@ -101,18 +99,16 @@ func New(name string, perm os.FileMode) (*Server, error) {
 	}
 
 	header := (*shmHeader)(unsafe.Pointer(&data[0]))
-	header.version = 1
+	header.Version = 1
 
 	lock := header.rwLocker()
 	lock.Create()
 
 	lock.Lock()
 
-	header.ip4.base = C.size_t(ip4BasePos)
-	header.ip6.base = C.size_t(ip6BasePos)
-	header.ip6route.base = C.size_t(ip6rBasePos)
+	header.setBlocks(ip4BasePos, 0, ip6BasePos, 0, ip6rBasePos, 0)
 
-	header.revision = 1
+	header.Revision = 1
 
 	lock.Unlock()
 
@@ -162,16 +158,9 @@ func (s *Server) commit() error {
 
 	lock.Lock()
 
-	header.ip4.base = C.size_t(ip4BasePos)
-	header.ip4.len = C.size_t(len(s.ip4s.Data))
+	header.setBlocks(ip4BasePos, len(s.ip4s.Data), ip6BasePos, len(s.ip6s.Data), ip6rBasePos, len(s.ip6rs.Data))
 
-	header.ip6.base = C.size_t(ip6BasePos)
-	header.ip6.len = C.size_t(len(s.ip6s.Data))
-
-	header.ip6route.base = C.size_t(ip6rBasePos)
-	header.ip6route.len = C.size_t(len(s.ip6rs.Data))
-
-	header.revision++
+	header.Revision++
 
 	lock.Unlock()
 
@@ -181,11 +170,9 @@ func (s *Server) commit() error {
 
 	lock.Lock()
 
-	header.ip4.base = C.size_t(ip4BasePos2)
-	header.ip6.base = C.size_t(ip6BasePos2)
-	header.ip6route.base = C.size_t(ip6rBasePos2)
+	header.setBlocks(ip4BasePos2, len(s.ip4s.Data), ip6BasePos2, len(s.ip6s.Data), ip6rBasePos2, len(s.ip6rs.Data))
 
-	header.revision++
+	header.Revision++
 
 	if err = s.file.Truncate(int64(size2)); err != nil {
 		lock.Unlock()
@@ -520,8 +507,8 @@ func (s *Server) Count() (ip4, ip6, ip6routes int, err error) {
 
 	header := (*shmHeader)(unsafe.Pointer(&s.data[0]))
 
-	ip4 = int(header.ip4.len / net.IPv4len)
-	ip6 = int(header.ip6.len / net.IPv6len)
-	ip6routes = int(header.ip6route.len / (net.IPv6len / 2))
+	ip4 = int(header.IP4.Len / net.IPv4len)
+	ip6 = int(header.IP6.Len / net.IPv6len)
+	ip6routes = int(header.IP6Route.Len / (net.IPv6len / 2))
 	return
 }

.gitignore
@@ -1,0 +1,27 @@
+_obj
+
+# Numerous always-ignore extensions
+*.diff
+*.err
+*.orig
+*.log
+*.rej
+*.swo
+*.swp
+*.zip
+*.vi
+*~
+
+# OS or Editor folders
+.DS_Store
+._*
+Thumbs.db
+.cache
+.project
+.settings
+.tmproj
+*.esproj
+nbproject
+*.sublime-project
+*.sublime-workspace
+.idea

blocker_linux.go
@@ -1,0 +1,11 @@
+// Copyright 2016 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+// +build linux
+
+//go:generate sh -c "GOARCH=386 go tool cgo -godefs blocker.go | gofmt > blocker_linux_386.go"
+//go:generate sh -c "GOARCH=amd64 go tool cgo -godefs blocker.go | gofmt > blocker_linux_amd64.go"
+
+package blocker

mutex.go
@@ -5,29 +5,27 @@
 
 package blocker
 
-//#include "ngx_ip_blocker_shm.h"
-import "C"
-
 import "github.com/tmthrgd/go-sem"
 
-type mutex C.ngx_ip_blocker_mutex_st
+// type mutex C.ip_blocker_mutex_st
+// 	see blocker.go
 
 func (m *mutex) Create() {
-	sem := (*sem.Semaphore)(&m.sem)
+	sem := (*sem.Semaphore)(&m.Sem)
 	if err := sem.Init(1); err != nil {
 		panic(err)
 	}
 }
 
 func (m *mutex) Lock() {
-	sem := (*sem.Semaphore)(&m.sem)
+	sem := (*sem.Semaphore)(&m.Sem)
 	if err := sem.Wait(); err != nil {
 		panic(err)
 	}
 }
 
 func (m *mutex) Unlock() {
-	sem := (*sem.Semaphore)(&m.sem)
+	sem := (*sem.Semaphore)(&m.Sem)
 	if err := sem.Post(); err != nil {
 		panic(err)
 	}

commit 4adb9f020956345d87199411947107c849f6e693

Fix documentation errors caused by 78fb4a2

blocker.go
@@ -5,10 +5,6 @@
 
 // +build !linux !386,!amd64
 
-// Package blocker is an efficient shared memory IP
-// blocking system for nginx.
-//
-// See https://github.com/tmthrgd/nginx-ip-blocker
 package blocker
 
 /*
@@ -44,12 +40,6 @@ typedef struct {
 } ip_blocker_shm_st;
 */
 import "C"
-
-import "errors"
-
-// ErrClosed will be returned on attempts to call
-// methods on a closed server or client.
-var ErrClosed = errors.New("shared memory closed")
 
 type mutex C.ip_blocker_mutex_st
 

blocker_linux_386.go
@@ -3,10 +3,6 @@
 
 package blocker
 
-import "errors"
-
-var ErrClosed = errors.New("shared memory closed")
-
 type mutex struct {
 	Sem [16]byte
 }

blocker_linux_amd64.go
@@ -3,10 +3,6 @@
 
 package blocker
 
-import "errors"
-
-var ErrClosed = errors.New("shared memory closed")
-
 type mutex struct {
 	Sem [32]byte
 }

client.go
@@ -6,15 +6,12 @@
 package blocker
 
 import (
-	"errors"
 	"net"
 	"os"
 	"sync"
 	"syscall"
 	"unsafe"
 )
-
-var errInvalidSharedMem = errors.New("invalid shared memory")
 
 // Client is an IP blocker shared memory client.
 type Client struct {

doc.go
@@ -1,0 +1,10 @@
+// Copyright 2016 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+// Package blocker is an efficient shared memory IP
+// blocking system for nginx.
+//
+// See https://github.com/tmthrgd/nginx-ip-blocker
+package blocker

errors.go
@@ -1,0 +1,26 @@
+// Copyright 2016 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+package blocker
+
+import "errors"
+
+var (
+	// ErrClosed will be returned on attempts to call
+	// methods on a closed server or client.
+	ErrClosed = errors.New("shared memory closed")
+
+	// ErrAlreadyBatching will be returned on attempts to call
+	// (*Server).Batch() when the *Server is already
+	// batching.
+	ErrAlreadyBatching = errors.New("already batching")
+
+	// ErrNotBatching will be returned on attempts to call
+	// (*Server).Commit() when (*Server).Batch() has
+	// not previously been called.
+	ErrNotBatching = errors.New("not batching")
+
+	errInvalidSharedMem = errors.New("invalid shared memory")
+)

server.go
@@ -6,24 +6,11 @@
 package blocker
 
 import (
-	"errors"
 	"net"
 	"os"
 	"sync"
 	"syscall"
 	"unsafe"
-)
-
-var (
-	// ErrAlreadyBatching will be returned on attempts to call
-	// (*Server).Batch() when the *Server is already
-	// batching.
-	ErrAlreadyBatching = errors.New("already batching")
-
-	// ErrNotBatching will be returned on attempts to call
-	// (*Server).Commit() when (*Server).Batch() has
-	// not previously been called.
-	ErrNotBatching = errors.New("not batching")
 )
 
 const cachelineSize = 64